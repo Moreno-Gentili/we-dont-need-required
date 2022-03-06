96845e6be4f0a3f67b6117e90369a13a41dc0a95
Moreno Gentili <moreno.gentili@codiceplastico.com>
2022-03-06 17:56:26 +0100

    Get better benchmark measurements by using datflow classes

--- src/WeDontNeedRequired.Benchmark/Program.cs
+++ src/WeDontNeedRequired.Benchmark/Program.cs
@@ -1,5 +1,5 @@
-﻿using System.Text;
-using System.Text.Json;
+﻿using System.Net.Http.Json;
+using System.Threading.Tasks.Dataflow;
 using AutoFixture;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
@@ -15,7 +15,10 @@ BenchmarkRunner.Run<RequireProperties>();
 public class RequireProperties
 {
     private HttpClient? httpClient;
-    private HttpContent? content;
+    private TimeTravelConfiguration? model;
+    private BufferBlock<HttpRequestMessage>? requestQueue;
+    public ActionBlock<HttpRequestMessage>? requestExecutor;
+    public int requestsPerIteration = 50000;
 
     [Params(DeserializationMode.SystemTextJson,
             DeserializationMode.NewtonsoftJson,
@@ -27,32 +30,56 @@ public class RequireProperties
     public int DegreeOfParallelism;
 
     [GlobalSetup]
-    public void Setup()
+    public void GlobalSetup()
     {
         httpClient = CreateClient();
-        TimeTravelConfiguration model = GetTimeTravelConfigurationModel();
-        string payload = JsonSerializer.Serialize(model);
-        content = new StringContent(payload, Encoding.UTF8, "application/json");
+        model = GetTimeTravelConfigurationModel();
+    }
+
+    [IterationSetup]
+    public void IterationSetup()
+    {
+        requestQueue = new BufferBlock<HttpRequestMessage>();
+        for (int i = 1; i <= requestsPerIteration; i++)
+        {
+            requestQueue.Post(CreateRequest());
+        }
+
+        ExecutionDataflowBlockOptions executionOptions = new (){ MaxDegreeOfParallelism = DegreeOfParallelism };
+        requestExecutor = new ActionBlock<HttpRequestMessage>(PerformRequest, executionOptions);
+    }
+
+    [GlobalCleanup]
+    public void GlobalCleanup()
+    {
+        httpClient!.Dispose();
     }
 
     [Benchmark]
-    public async Task Request()
+    public Task MeasureDeserializerPerformance()
     {
-        /*List<Task> tasks = Enumerable.Range(1, 50000).Select(counter => PerformRequest()).ToList();
-        await Task.WhenAll(tasks);*/
-        
-
-        for (int i = 0; i < 1152 * 30 / DegreeOfParallelism; i++)
+        if (requestQueue is null || requestExecutor is null)
         {
-            List<Task> tasks = Enumerable.Range(1, DegreeOfParallelism).Select(counter => PerformRequest()).ToList();
-            await Task.WhenAll(tasks);
+            throw new InvalidOperationException("Requests are not available");
         }
+
+        requestQueue.LinkTo(requestExecutor, new DataflowLinkOptions { PropagateCompletion = true });
+        requestQueue.Complete();
+        return requestExecutor.Completion;
     }
 
-    private async Task PerformRequest()
+    private async Task PerformRequest(HttpRequestMessage request)
     {
-        HttpResponseMessage response = await httpClient!.PostAsync("/TimeTravel/Configure", content);
+        HttpResponseMessage response = await httpClient!.SendAsync(request);
         response.EnsureSuccessStatusCode();
+    }
+
+    private HttpRequestMessage CreateRequest()
+    {
+        return new HttpRequestMessage(HttpMethod.Post, "/TimeTravel/Configure")
+        {
+            Content = JsonContent.Create(model)
+        };
     }
 
     private HttpClient CreateClient()
